d3db38ce457e189ec6b0fbbcd62118cfd2136bde
saba temitayo <sabalearning01@gmail.com>
2025-06-19 21:26:19 +0100

    hamburger toggle

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,104 +1,3 @@
-// import React, { useState } from "react";
-// import { LuTextSearch } from "react-icons/lu";
-// import { MdArrowDropDown } from "react-icons/md";
-// import { IoMenuOutline } from "react-icons/io5";
-// const Header = () => {
-//   const [menuOpen, setIsMenuOpen] = useState(false);
-//   const [selectOption, setSelectOption] = useState("Explore Services");
-
-//   const Toggle = () => {
-//     setIsMenuOpen((prevState) => !prevState);
-//   };
-
-//   const handleSelectOption = (option) => {
-//     setSelectOption(option);
-//     console.log(option);
-//   };
-
-//   return (
-//     <div>
-//       <div className="w-[100%] h-[92px] bg-[#004646]">
-//         <div className="flex justify-between items-center pt-[20px]   lg:ml-[100px] lg:mr-[100px]  ">
-//           <div className="flex justify-center items-center  ml-2.5">
-//             <LuTextSearch className="text-white text-2xl lg:text-3xl cursor-pointer" />
-//             <h6 className="font-[DM Sans]  cursor-pointer font-bold text-[17.15px] text-[#E6F2F2] lg:text-[28.45px]">
-//               Findartisan
-//             </h6>
-//             <div className="hidden md:block lg:block ml-[35px] mt-[5px]">
-//               <div className="relative lg:block md:hidden inline-block text-left group">
-//                 <button className="inline-flex items-center text-white font-semibold">
-//                   Explore Services
-//                   <MdArrowDropDown className="text-[#FDFDFD] mt-[5px]" />
-//                 </button>
-
-//                 <div className="hidden group-hover:block absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-md z-10">
-//                   <a
-//                     href="#"
-//                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-//                     onClick={() => handleSelectOption("Web Development")}
-//                   >
-//                     Web Development
-//                   </a>
-//                   <a
-//                     href="#"
-//                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-//                     onClick={() => handleSelectOption("Graphic Design")}
-//                   >
-//                     Graphic Design
-//                   </a>
-//                   <a
-//                     href="#"
-//                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-//                     onClick={() => handleSelectOption("SEO Services")}
-//                   >
-//                     SEO Services
-//                   </a>
-
-//                   {/* {selectOption && (
-//                     <p className="mt-4 text-green-700">
-//                       You selected: <strong>{selectOption}</strong>
-//                     </p>
-//                   )} */}
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="flex justify-center items-center gap-[12px] mr-2.5  ">
-//             <button className="font-[DM Sans] text-[16px] cursor-pointer rounded-lg text-[#FDFDFD] font-medium py-[10px] px-[16px]  border-1 border-white lg:hidden ">
-//               Hire an Artisan
-//             </button>
-
-//             <button className="hidden md:hidden lg:block font-[DM Sans] cursor-pointer text-[16px] rounded-lg text-[#FDFDFD] font-medium py-[14px] px-[32px]  border-1 border-white">
-//               Hire an Artisan
-//             </button>
-//             <IoMenuOutline
-//               onClick={Toggle}
-//               className="text-white text-2xl  sm:block md:block lg:hidden"
-//             />
-
-//             {/* Conditional rendering */}
-//             {menuOpen && (
-//               <div
-//                 onClick={Toggle}
-//                 className="bg-black text-white w-[80%] h-[250px] absolute top-20 left-0  flex justify-center items-center z-40"
-//               >
-//                 <h3>Great</h3>
-//               </div>
-//             )}
-
-//             <button className="hidden md:hidden lg:block font-[DM Sans] cursor-pointer bg-[#CC5500] text-[16px] rounded-lg text-[#FDFDFD] font-medium py-[14px] px-[32px]">
-//               Become an Artisan
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Header;
-
-
 
 import React, { useState } from "react";
 import { LuTextSearch } from "react-icons/lu";
@@ -192,9 +91,14 @@ const Header = () => {
             {menuOpen && (
               <div
                 onClick={Toggle}
-                className="bg-black text-white w-[80%] h-[250px] absolute top-20 left-0 flex justify-center items-center z-40"
+                className="bg-white text-black w-[90%] h-[250px] absolute top-24 rounded-lg left-0 ml-[16px] mr-[16px]  z-40"
               >
-                <h3>Great</h3>
+                <a href="/"><p className="pl-[10px] pt-[16px]">Home Cleaning</p></a>
+                <a href="/"><p className="pl-[10px] pt-[16px]">Plumbing</p></a>
+                <a href="/"><p className="pl-[10px] pt-[16px]">Electrical</p></a>
+                <a href="/"><p className="pl-[10px] pt-[16px]">Carpentry</p></a>
+                <a href="/"><p className="pl-[10px] pt-[16px]">Gardening</p></a>
+                <a href="/"><p className="pl-[10px] pt-[16px]">Painting</p></a>
               </div>
             )}
 
