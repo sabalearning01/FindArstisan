0da3ecdd6c2eed60033a0f6042611b1129659cbc
saba temitayo <sabalearning01@gmail.com>
2025-06-19 09:47:40 +0100

    cleanup

--- src/Pages/Admin/Order.jsx
+++ src/Pages/Admin/Order.jsx
@@ -1,24 +1,3 @@
-// import React from 'react'
-// import { useNavigate } from 'react-router-dom'
-// import AdminSearch from '../../Components/AdminSearch';
-
-// const Order = () => {
-//   const navigate = useNavigate();
-
-//   const handleNavigation = () => {
-//     navigate('/dashboard/order');
-//   };
-
-//   return (
-//     <div>
-//       <AdminSearch/>
-//       <h1>Order</h1>
-//     </div>
-//   )
-// }
-
-// export default Order
-
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import AdminSearch from "../../Components/AdminSearch";
